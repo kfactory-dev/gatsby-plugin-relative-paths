ca44625869bf30fb10cfe2685ff5e52bd41537ae
Miguel Savignano <migue.masx@gmail.com>
2020-10-25 19:51:37 +0100

    first commit

--- README.md
+++ README.md
@@ -1,39 +1,26 @@
-# gatsby-plugin-ipfs
+# gatsby-plugin-relative-paths
 
-[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
+[![NPM version][npm-image]][npm-url]
 
-[npm-url]:https://npmjs.org/package/gatsby-plugin-ipfs
-[npm-image]:https://img.shields.io/npm/v/gatsby-plugin-ipfs.svg
-[downloads-image]:https://img.shields.io/npm/dm/gatsby-plugin-ipfs.svg
-[david-dm-url]:https://david-dm.org/moxystudio/gatsby-plugin-ipfs
-[david-dm-image]:https://img.shields.io/david/moxystudio/gatsby-plugin-ipfs.svg
-[david-dm-dev-url]:https://david-dm.org/moxystudio/gatsby-plugin-ipfs?type=dev
-[david-dm-dev-image]:https://img.shields.io/david/dev/moxystudio/gatsby-plugin-ipfs.svg
-
-Adds support for deploying [Gatsby](https://www.gatsbyjs.org/) websites to [IPFS](https://ipfs.io/) by ensuring that assets are relative.
-
+[npm-url]: https://npmjs.org/package/gatsby-plugin-relative-paths
+[npm-image]: https://img.shields.io/npm/v/gatsby-plugin-relative-paths.svg
 
 ## Installation
 
 ```sh
-$ npm install --save gatsby-plugin-ipfs
+npm install --save gatsby-plugin-relative-paths
 ```
-
 
 ## Usage
 
-Set `prefixPath` to `__GATSBY_IPFS_PATH_PREFIX__` and include the plugin in your `gatsby-config.js` file:
+Set `prefixPath` to `__GATSBY_RELATIVE_PATH_PREFIX__` and include the plugin in your `gatsby-config.js` file:
 
 ```js
 module.exports = {
-    pathPrefix: '__GATSBY_IPFS_PATH_PREFIX__',
-    plugins: [
-        'gatsby-plugin-ipfs',
-    ],
+  pathPrefix: '__GATSBY_RELATIVE_PATH_PREFIX__',
+  plugins: ['gatsby-plugin-relative-paths'],
 };
 ```
-
-And now, simply build the project with `npm run build -- --prefix-paths`. Better yet, set it by default in your `package.json`:
 
 ```json
 "scripts": {
@@ -41,14 +28,11 @@ And now, simply build the project with `npm run build -- --prefix-paths`. Better
 },
 ```
 
-
 ## But how?
 
 It turns out the Gatsby doesn't support relative paths. But I didn't gave up and came up with smart and ugly hacks to do so:
 
-- Adds a post-build step that iterates over files and transforms every `__GATSBY_IPFS_PATH_PREFIX__` occurrence
-- Adds a very small code snippet to every HTML page that defines the `__GATSBY_IPFS_PATH_PREFIX__` global based on the browser location
-
+- Adds a post-build step that iterates over files and transforms every `__GATSBY_RELATIVE_PATH_PREFIX__` occurrence
 
 ## License
 
---
+++ src/relative-paths.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const util = require('util');
+const pMap = require('p-map');
+const globby = require('globby');
+const isTextPath = require('is-text-path');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const TRANSFORM_CONCURRENCY = 10;
+const PATH_PREFIX = '__GATSBY_RELATIVE_PATH_PREFIX__';
+
+function getRelativePrefix(path) {
+  const depth = path.split('/').length - 2;
+  return depth > 0 ? '../'.repeat(depth) : './';
+}
+exports.getRelativePrefix = getRelativePrefix;
+
+/**
+Replaces all /__GATSBY_RELATIVE_PATH_PREFIX__/ strings with the correct relative paths
+based on the depth of the file within the `public/` folder
+*/
+async function relativizeHtmlFiles(assetPrefix) {
+  const paths = await globby(['public/**/*.html']);
+
+  await pMap(
+    paths,
+    async (path) => {
+      let contents = await readFileAsync(path, 'utf-8');
+      if (!contents.includes(PATH_PREFIX)) return;
+
+      const relativePrefix = getRelativePrefix(path);
+
+      contents = contents.replace(
+        new RegExp(`/${assetPrefix}/${PATH_PREFIX}/`, 'g'),
+        `${relativePrefix}${assetPrefix}/`
+      );
+      contents = contents.replace(new RegExp(`/${PATH_PREFIX}/`, 'g'), `${relativePrefix}`);
+
+      await writeFileAsync(path, contents);
+    },
+    { concurrency: TRANSFORM_CONCURRENCY }
+  );
+}
+exports.relativizeHtmlFiles = relativizeHtmlFiles;
+
+/**
+Replaces all "/__GATSBY_RELATIVE_PATH_PREFIX__" strings __GATSBY_RELATIVE_PATH_PREFIX__
+Replaces all "/__GATSBY_RELATIVE_PATH_PREFIX__/" strings with __GATSBY_RELATIVE_PATH_PREFIX__ + "/"
+Replaces all "/__GATSBY_RELATIVE_PATH_PREFIX__/xxxx" strings with __GATSBY_RELATIVE_PATH_PREFIX__ + "/xxxx"
+Also ensures that `__GATSBY_RELATIVE_PATH_PREFIX__` is defined in case this JS file is outside the document context, e.g.: in a worker
+*/
+async function relativizeJsFiles() {
+  const paths = await globby(['public/**/*.js']);
+
+  await pMap(
+    paths,
+    async (path) => {
+      let contents = await readFileAsync(path, 'utf-8');
+      if (!contents.includes(PATH_PREFIX)) return;
+
+      contents = contents
+        .replace(/["']\/__GATSBY_RELATIVE_PATH_PREFIX__['"]/g, () => ' __GATSBY_RELATIVE_PATH_PREFIX__ ')
+        .replace(
+          /(["'])\/__GATSBY_RELATIVE_PATH_PREFIX__\/([^'"]*?)(['"])/g,
+          (matches, g1, g2, g3) => ` ${PATH_PREFIX} + ${g1}/${g2}${g3}`
+        );
+
+      contents = `if(typeof ${PATH_PREFIX} === 'undefined'){${PATH_PREFIX}=''}${contents}`;
+
+      await writeFileAsync(path, contents);
+    },
+    { concurrency: TRANSFORM_CONCURRENCY }
+  );
+}
+exports.relativizeJsFiles = relativizeJsFiles;
+
+/**
+Replaces all /__GATSBY_RELATIVE_PATH_PREFIX__/ strings to standard relative paths
+*/
+async function relativizeMiscAssetFiles() {
+  const paths = await globby(['public/**/*', '!public/**/*.html', '!public/**/*.js']);
+
+  await pMap(
+    paths,
+    async (path) => {
+      // Skip if is a binary file
+      if (!isTextPath(path)) return;
+
+      let contents = await readFileAsync(path, 'utf-8');
+      if (!contents.includes(PATH_PREFIX)) return;
+
+      contents = contents.replace(new RegExp(`/${PATH_PREFIX}/`, 'g'), getRelativePrefix(path));
+
+      await writeFileAsync(path, contents);
+    },
+    { concurrency: TRANSFORM_CONCURRENCY }
+  );
+}
+exports.relativizeMiscAssetFiles = relativizeMiscAssetFiles;
