40d864920060ee68c0f61dba20480a04d390fa11
Miguel Savignano <migue.masx@gmail.com>
2020-10-25 20:42:44 +0100

    fix prefix path error

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ const {
 exports.onPreBootstrap = ({ store, reporter }) => {
   const { config, program } = store.getState();
   reporter.panic(`The pathPrefix must be set to ${PATH_PREFIX} in your gatsby-config.js file`);
-  if (config.pathPrefix === '__GATSBY_RELATIVE_PATH_PREFIX__') {
+  if (config.pathPrefix !== '__GATSBY_RELATIVE_PATH_PREFIX__') {
     reporter.panic(`The pathPrefix must be set to ${PATH_PREFIX} in your gatsby-config.js file`);
   }
 
